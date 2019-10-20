50b871513cc9c3c68e5e40186e8dbfd90edd5ce6
Kaiisha05 <Kaiisha@yahoo.com>
2019-10-20 17:42:14 -0400

    firebase connected. user input displayed in firebase and webpage. User input replacing,not staying appened though
    git push origin master

--- assets/javascript/java.js
+++ assets/javascript/java.js
@@ -6,7 +6,48 @@ var firebaseConfig = {
     storageBucket: "",
 };
 
-firebase.initializeApp(config);
+firebase.initializeApp(firebaseConfig);
 
 // Create a variable to reference the database
 var database = firebase.database();
+var trainName;
+var destination;
+// var firstTrain;
+// var frequency;
+// At the initial load and subsequent value changes, get a snapshot of the stored data.
+// This function allows you to update your page in real-time when the firebase database changes.
+// database.ref().on("value", function (snapshot) {
+
+
+//     $("#user-train-name").text(trainName);
+//     $("#user-destination").text(destination);
+
+
+$("#submit").on("click", function () {
+    event.preventDefault();
+
+    // variables needed to populate the user input fields
+    trainName = $("#train-name").val().trim();
+    destination = $("#destination").val().trim();
+    // firstTrain = $("#1st-train").val().trim();
+    // frequency = $("#frequency").val().trim();
+
+  
+
+        console.log(trainName);
+        console.log(destination);
+        // console.log(firstTrain);
+        // console.log(frequency);
+ 
+        database.ref().set({
+            train: trainName,
+            dest: destination,
+        });
+
+        // user input displayed on page
+$("#user-train-name").text(trainName);
+$("#user-destination").text(destination);
+        // })
+
+   
+    });
